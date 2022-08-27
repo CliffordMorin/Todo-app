5af721c2d65bb526c0a2a606c4b267e0e43524d0
Clifford Morin <cemorin21@gmail.com>
2022-08-26 20:20:59 -0400

    added header styling and making form styling

--- src/MyList/MyList.js
+++ src/MyList/MyList.js
@@ -3,11 +3,25 @@ import "./mylist.css";
 const MyList = () => {
   return (
     <div className="listContainer">
+      <header className="listHeader">My Todo List</header>
       <div className="listBackground">
         <div className="listShape"></div>
         <div className="listShape"></div>
       </div>
-      <form className="form"></form>
+
+      <form className="form">
+        <input type="text" className="todo-input" />
+        <button className="todo-button" type="submit">
+          <i className="fas fa-plus-square"></i>
+        </button>
+        <div className="select">
+          <select name="todos" className="filter-todo">
+            <option value="all">All</option>
+            <option value="completed">Completed</option>
+            <option value="uncompleted">Uncompleted</option>
+          </select>
+        </div>
+      </form>
     </div>
   );
 };
